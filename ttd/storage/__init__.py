a200625b418a402153dfc465704b950527fb888b
Mathieu Crilout <mathieu.crilout@gmail.com>
2025-03-31 19:33:45 +0200

    reorganize storage

--- ttd/storage/__init__.py
+++ ttd/storage/__init__.py
@@ -4,7 +4,7 @@ Backed by pluggable storage implementations (e.g., TinyDB).
 """
 
 from .base import StorageService, TinyDBStorageService
-from .ttd import TTDStorage
+from .ttd_storage import TTDStorage
 
 __all__ = [
     "StorageService",
