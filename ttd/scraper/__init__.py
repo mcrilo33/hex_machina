5d88f214a6f606ca6f86abdb5d6c76763d9ed9bd
Mathieu Crilout <mathieu.crilout@gmail.com>
2025-03-31 19:39:10 +0200

    Handle extract_domain in scraper module

--- ttd/scraper/__init__.py
+++ ttd/scraper/__init__.py
@@ -5,6 +5,7 @@ Includes base classes and specialized scrapers (e.g., RSS, HTML).
 
 from .base_article import BaseArticleScraper
 from .rss_article import RSSArticleScraper
+from .utils import extract_domain, extract_markdown_from_html
 
 __all__ = [
     "BaseArticleScraper",
