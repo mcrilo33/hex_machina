850b13d9a9dcd52bae1d7d41e098ffbad9286eb7
Mathieu Crilout <mathieu.crilout@gmail.com>
2025-06-26 18:16:42 +0200

    Introduce an example to run the generate_newsletter.py

--- run_generate_newsletter.sh
+++ run_generate_newsletter.sh
@@ -2,15 +2,14 @@
 # -- run_generate_newsletter.sh ---------------------------------------------
 # Builds the current date in RFC-2822 format and launches the generator.
 
-source /Users/mathieucrilout/.miniconda3/bin/activate ttd
-export PYTHONPATH="/Users/mathieucrilout/Repos/hex_machina/hex:$PYTHONPATH"
-cd /Users/mathieucrilout/Repos/hex_machina || exit 1
+export PYTHONPATH="./hex:$PYTHONPATH"
 NOW_UTC="$(LC_TIME=C date -u -v-7d +"%a, %d %b %Y %H:%M:%S +0000")"
 echo "NOW_UTC: $NOW_UTC"
 
-exec /Users/mathieucrilout/Repos/hex_machina/generate_newsletter.py \
+python generate_newsletter.py \
   --ingestion-articles-table 'articles' \
   --replicates-table 'replicates' \
+  --articles-limit 100 \
   --date-threshold "${NOW_UTC}" \
   --selection-articles-limit 6 \
   --selected-articles-table 'selected_articles_dummy_table'
